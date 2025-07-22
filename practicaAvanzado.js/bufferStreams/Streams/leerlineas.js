7d5c0ff948d6f973d1c522a1d1ff0f6cffa26a21
saniagoValadez <saniago1340@gmail.com>
2025-07-22 14:00:43 -0600

    Se inicia a practicar con los event loop

--- practicaAvanzado.js/bufferStreams/Streams/leerlineas.js
+++ practicaAvanzado.js/bufferStreams/Streams/leerlineas.js
@@ -2,9 +2,9 @@ const fs = require ('node:fs')// importacion de los modulos. fs te permite manio
 const readLine = require ('node:readline') //Modulo que te permite leer lineas de texto una por una desde una fuente de entrada (como un archivo en consola)
 
 async function leerLineas() { // defines una función asincrona. Esto para poder usar await dentro de ella y manejar los flujos de datos (Streams) de manera mas clara y bloqueante.
-    const fileStream = fs.createReadStream('contenido.txt', 'utf8') // "fs.createReadStream" abre el archivo "contenido.txt" en modo lectura, pero no lo carga completo en memoria. Stream: Los datos se van cargando por partes (chunks), l que es eficiente para archivos grandes
+    const fileStream = fs.createReadStream('contenido.txt', 'utf8') // "fs.createReadStream" abre el archivo "contenido.txt" en modo lectura, pero no lo carga completo en memoria. Stream: Los datos se van cargando por partes (chunks), lo que es eficiente para archivos grandes
     const rl = readLine.createInterface({ // crea una interface de lectura por linea
-        input: fileStream, // le decimos que la fuente de datos, sera el archivo que abrimos
+        input: fileStream, // le decimos que la fuente de datos, sera el archivo que abrimos. "Como se hace referencia ene la linea 5"
         crlfDelay: Infinity // Asegura que las lineas se reconozcan correctamente, incluso si tienen saltos de linea. lo que ecita que se rompan en medio.
     })
     for await (const line of rl){ // este "for await of" es especial para iterar sobre streams asincronos. Casa "line", representa una linea completa del archivo.
